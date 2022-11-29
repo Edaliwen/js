d337bc9ea483162b895e1cd928cc77a68761bfdc
Edaliwen <amandine.valtat@gmail.com>
2022-11-29 16:42:39 +0100

    Exercice 5
    Ma solution

---
+++ assets/js/exercice5.js
@@ -0,0 +1,24 @@
+let result = 0
+let add = document.querySelector("#add")
+let subtract = document.querySelector("#sub")
+
+let selectButtonAdd = document.querySelector("#add")
+let selectButtonSubtract = document.querySelector("#sub")
+selectButtonAdd.addEventListener("click", add1);
+selectButtonSubtract.addEventListener("click", sub1);
+
+document.querySelector(".result").innerHTML = result
+
+function add1(){
+    result++
+    displayResult(result)
+}
+
+function sub1(){
+    result--
+    displayResult(result)
+}
+
+function displayResult(result){
+    document.querySelector(".result").innerHTML = result
+}
