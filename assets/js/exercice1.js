904f24e55957a4df307d9b3f844e81fe21258f8e
Edaliwen <amandine.valtat@gmail.com>
2022-11-29 09:16:35 +0100

    Correction exercices 1 & 2

---
+++ assets/js/exercice1.js
@@ -0,0 +1,8 @@
+let nb = 6;
+alert(nb);
+
+let entier;
+function afficherUnNombreEntier(entier) {
+    alert(entier);
+}
+afficherUnNombreEntier(6);
---
+++ assets/js/exercice2.js
@@ -0,0 +1,7 @@
+let lastName = "VALTAT";
+let firstName ="Amandine";
+let city = "Maurepas";
+
+document.querySelector("#lastName").value = lastName;
+document.querySelector("#firstName").value = firstName;
+document.querySelector("#city").value = city;
